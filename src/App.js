174b0dfb744eb0361b73d39c8f3df757091bdf71
tudorale <tudorale2005@gmail.com>
2021-04-10 16:51:02 -1200

    first version

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
   const Main = React.lazy(() => import("./components/Main"))
+  const Privacy = React.lazy(() => import("./components/Privacy"))
+  const Terms = React.lazy(() => import("./components/Terms"))
+
   return (
     <Suspense fallback={<div className="loadingSpinner"></div>}>
       <div>
         <Router>
           <Route path={process.env.PUBLIC_URL + '/'} exact component={Main}/>
+          <Route path={process.env.PUBLIC_URL + '/policy'} exact component={Privacy}/>
+          <Route path={process.env.PUBLIC_URL + '/terms'} exact component={Terms}/>
         </Router>
       </div>
     </Suspense>
